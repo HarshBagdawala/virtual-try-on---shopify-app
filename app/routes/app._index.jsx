04420fda84f41da1ded095c758fde5aabfeddf15
HarshBagdawala <harshbagdawala@gmail.com>
2026-04-20 11:33:01 +0530

    first commit2

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -89,6 +89,7 @@ export default function Index() {
                 content: 'Enable App Block',
                 url: themeEditUrl,
                 external: true,
+                target: '_blank',
               }}
             >
               <p>
